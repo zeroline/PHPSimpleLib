3465e9e2f668410b4f0ce9fd0a8f5bc970b32e7b
zeroline <fred@zeroline.me>
2021-01-03 20:38:29 +0100

    Added namespace / class extractor

--- src/Core/Controlling/NamespaceExtractor.php
+++ src/Core/Controlling/NamespaceExtractor.php
@@ -54,4 +54,44 @@ final class NamespaceExtractor
         }
         return null;
     }
+
+    /**
+     * 
+     * @param string $filename 
+     * @return array 
+     */
+    public static function byFile(string $filename) : array {
+        $fp = fopen($filename, 'r');
+        $class = $namespace = $buffer = '';
+        $i = 0;
+        while (!$class) {
+            if (feof($fp)) break;
+
+            $buffer .= fread($fp, 512);
+            $tokens = token_get_all($buffer);
+
+            if (strpos($buffer, '{') === false) continue;
+
+            for (;$i<count($tokens);$i++) {
+                if ($tokens[$i][0] === T_NAMESPACE) {
+                    for ($j=$i+1;$j<count($tokens); $j++) {
+                        if ($tokens[$j][0] === T_STRING) {
+                            $namespace .= '\\'.$tokens[$j][1];
+                        } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                            break;
+                        }
+                    }
+                }
+
+                if ($tokens[$i][0] === T_CLASS) {
+                    for ($j=$i+1;$j<count($tokens);$j++) {
+                        if ($tokens[$j] === '{') {
+                            $class = $tokens[$i+2][1];
+                        }
+                    }
+                }
+            }
+        }
+        return array($namespace, $class);
+    }
 }
